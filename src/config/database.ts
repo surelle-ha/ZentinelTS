5ee2049229d27ae4b865d2e66701317cb4e143ae
surelle-ha <bsit.eustahc@gmail.com>
2024-08-05 21:46:33 +0800

    UPD: Cleanup & New Conf

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,17 +9,16 @@ const dbPort: number = Number(process.env.DATABASE_PORT) || 3306;
 const dbType: string = process.env.DATABASE_TYPE || "mysql";
 
 export default async function setupDatabase(app: Express): Promise<void> {
-    app.z.sequelize = new Sequelize(dbName, dbUser, dbPass, {
-        host: dbHost,
-        port: dbPort,
-        dialect: dbType as "mysql" | "postgres" | "sqlite" | "mssql",
-        logging: false,
-    });
-
-    try {
-        await app.z.sequelize.authenticate();
-        app.z.logger.info(`Database connected successfully`);
-    } catch (error) {
-        app.z.logger.error(`[SQL] Connection Error`, error);
-    }
+	app.z.sequelize = new Sequelize(dbName, dbUser, dbPass, {
+		host: dbHost,
+		port: dbPort,
+		dialect: dbType as "mysql" | "postgres" | "sqlite" | "mssql",
+		logging: false,
+	});
+	try {
+		await app.z.sequelize.authenticate();
+		app.z.logger.info(`Database connected successfully`);
+	} catch (error) {
+		app.z.logger.error(`[SQL] Connection Error`, error);
+	}
 }
